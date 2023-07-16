ebe15408a61d0dede11e7756487d5d686a4a9899
Kirito071008 <107003946+Kirito071008@users.noreply.github.com>
2023-07-17 00:22:08 +0200

    Update Age Calculator.py

--- Age Calculator.py
+++ Age Calculator.py
@@ -6,9 +6,9 @@ def age_calculator(birthday):
     age_in_years = differnce.days // 365
     return age_in_years
 
-year = int(input("Inserisci l'anno di nascita (YYYY): "))
-month = int(input("Inserisci il mese di nascita (MM): "))
-day = int(input("Inserisci il giorno di nascita (DD): "))
+year = int(input("Birth Year (YYYY): "))
+month = int(input("Birth Month (MM): "))
+day = int(input("Birth Day (DD): "))
 
 birthday = datetime.date(year, month, day)
 age = age_calculator(birthday)
